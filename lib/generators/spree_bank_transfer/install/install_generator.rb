ea7cfb85cdba73324ba11bcf6f66a6f45ec40449
Vincent K Edison <vincent@luxodev.com>
2018-04-06 19:36:29 +0700

    modify generator

--- lib/generators/spree_bank_transfer/install/install_generator.rb
+++ lib/generators/spree_bank_transfer/install/install_generator.rb
@@ -10,7 +10,7 @@ module SpreeBankTransfer
       end
 
       def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_bank_transfer\n", before: /\*\//, verbose: true
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.scss', " *= require spree/frontend/spree_bank_transfer\n", before: /\*\//, verbose: true
         inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_bank_transfer\n", before: /\*\//, verbose: true
       end
 
