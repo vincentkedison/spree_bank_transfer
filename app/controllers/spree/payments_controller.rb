a07e0f2a5c6cea9b0b6ef49dffc224394e1114a1
Vincent K Edison <vk@luxodev.com>
2019-10-05 21:50:32 +0700

    bugfix

--- app/controllers/spree/payments_controller.rb
+++ app/controllers/spree/payments_controller.rb
@@ -4,20 +4,20 @@ module Spree
     before_action :find_payment
 
     def update
-      payment_details = PaymentDetails.new(@payment, payment_params)
+      payment_details = Spree::PaymentDetails.new(@payment, payment_params)
       if payment_details.save
         flash[:notice] = Spree.t(:payment_successfully_updated)
       else
         flash[:error] = payment_details.errors.to_sentence
       end
-      redirect_to :back
+      redirect_back(fallback_location: root_path)
     end
 
     def find_payment
       @payment = spree_current_user.payments.find_by(number: params[:id])
       unless @payment
         flash[:error] = Spree.t(:payment_not_found)
-        redirect_to :back
+        redirect_back(fallback_location: root_path)
       end
     end
 
